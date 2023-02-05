b6d880729b75e2aeb989376e457c4c5da6b5a462
Hynek Zamazal <zamazhyn@fel.cvut.cz>
2023-02-06 00:14:17 +0100

    started

---
+++ GUI/MainGUI.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MainGUI {
+    public JPanel mainPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JPanel statusPanel;
+    private JButton menuButton;
+    private JLabel TimeLabel;
+    private JButton lightButton;
+}
---
+++ HouseCtrPanel.java
@@ -0,0 +1,14 @@
+import GUI.MainGUI;
+
+import javax.swing.*;
+
+public class HouseCtrPanel {
+    public static void main(String[] args) {
+        JFrame mainWindow = new JFrame("Hebčákův mistrovský budík");
+        mainWindow.setContentPane(new MainGUI().mainPanel);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainWindow.pack();
+        mainWindow.setVisible(true);
+    }
+}
